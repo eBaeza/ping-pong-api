314c14086da4f9bb67d54667310add85f6371d9d
Edgar Baeza <holamundo@edgarbaeza.mx>
2018-03-30 18:17:32 -0600

    Init Game Model

---
+++ app/Controllers/Http/GameController.js
@@ -0,0 +1,20 @@
+'use strict'
+
+class GameController {
+  async index () {
+  }
+
+  async store () {
+  }
+
+  async show () {
+  }
+
+  async update () {
+  }
+
+  async destroy () {
+  }
+}
+
+module.exports = GameController
---
+++ database/migrations/1522450242539_games_schema.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class GamesSchema extends Schema {
+  up () {
+    this.create('games', table => {
+      table.increments()
+      table.integer('player_id').unsigned().notNullable()
+        .references('id').inTable('users')
+
+      table.integer('opponent_id').unsigned().notNullable()
+        .references('id').inTable('users')
+
+      table.integer('player_score').unsigned().defaultTo(0)
+      table.integer('opponent_score').unsigned().defaultTo(0)
+      table.string('result', 1).defaultTo('-')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('games')
+  }
+}
+
+module.exports = GamesSchema
